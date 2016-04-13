500ce1af7ed8b36fb8730a16bcab9eb1a83852b6
oluwatobi.okusanya <sanya_tobi@yahoo.com>
2016-04-13 14:08:47 +0100

    added commit message

--- index.js
+++ index.js
@@ -13,6 +13,13 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/webhook/', function (req, res) {
+  if (req.query['hub.verify_token'] === 'my_very_own_token') {
+    res.send(req.query['hub.challenge']);
+  }
+  res.send('Error, wrong validation token');
+})
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
