bf3385187dbebe8ac74a2db1ae8a3f0de8eff2a9
oluwatobi.okusanya <sanya_tobi@yahoo.com>
2016-04-13 14:56:34 +0100

    log

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.get('/webhook/', function (req, res) {
 });
 app.post('/webhook/', function (req, res) {
   console.log("fishermen");
-  console.log(req.body.entry);
+  console.log(req.body);
   return;
   messaging_events = req.body.entry[0].messaging;
   for (i = 0; i < messaging_events.length; i++) {
