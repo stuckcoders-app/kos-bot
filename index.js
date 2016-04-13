8b038a3447b1b52967926e3ac90d1cbd9363884c
oluwatobi.okusanya <sanya_tobi@yahoo.com>
2016-04-13 15:28:05 +0100

    edit package.json

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+
+
+
+// instruct the app to use the `bodyParser()` middleware for all routes
+app.use(bodyParser());
 
 var token = "CAAI8S9mStGQBAIK1SROK1uxYZA7mIv5mYcoX3ngHwYUCkriu11aRFXsoZAGb3kBAZAOhMTYFhztcBeZBFRoyyXuQZChZATj5CfELeeDZAy5NAcBkGwj01talblSb6IzozFGuIsw4mc74SKZBZBLZBjx8OR8xpduaVkivNjZA6A6dI3YNA9MQZBumsl2dAZByaH5atLQ0ZD";
 
@@ -43,9 +49,7 @@ app.get('/webhook/', function (req, res) {
   res.send('Error, wrong validation token');
 });
 app.post('/webhook/', function (req, res) {
-  console.log("fishermanss");
-  console.log(req);
-  return;
+
   messaging_events = req.body.entry[0].messaging;
   for (i = 0; i < messaging_events.length; i++) {
     event = req.body.entry[0].messaging[i];
