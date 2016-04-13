40f49daa61cc3a9b71f4e145ecffc2be4f27d623
oluwatobi.okusanya <sanya_tobi@yahoo.com>
2016-04-13 14:34:56 +0100

    log

--- index.js
+++ index.js
@@ -43,6 +43,8 @@ app.get('/webhook/', function (req, res) {
   res.send('Error, wrong validation token');
 });
 app.post('/webhook/', function (req, res) {
+  console.log(req.body);
+  return;
   messaging_events = req.body.entry[0].messaging;
   for (i = 0; i < messaging_events.length; i++) {
     event = req.body.entry[0].messaging[i];
