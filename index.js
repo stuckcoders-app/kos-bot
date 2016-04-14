2dff6a2dcd1dd80828cbfdaa06e47867284f5ad8
oluwatobi.okusanya <sanya_tobi@yahoo.com>
2016-04-14 10:52:02 +0100

    added mongo db

--- index.js
+++ index.js
@@ -6,6 +6,37 @@ var app = express();
 
 
 
+
+/**
+ * Created by oluwatobi.okusanya on 14/04/16.
+ */
+//lets require/import the mongodb native drivers.
+var mongodb = require('mongodb');
+
+//We need to work with "MongoClient" interface in order to connect to a mongodb server.
+var MongoClient = mongodb.MongoClient;
+
+// Connection URL. This is where your mongodb server is running.
+var url = 'mongodb://tobisanya:babapass1!@ds023540.mlab.com:23540/kos-shipping';
+
+// Use connect method to connect to the Server
+MongoClient.connect(url, function (err, db) {
+  if (err) {
+    console.log('Unable to connect to the mongoDB server. Error:', err);
+  } else {
+    //HURRAY!! We are connected. :)
+    console.log('Connection established to', url);
+
+    // do some work here with the database.
+
+    //Close connection
+    db.close();
+  }
+});
+
+
+
+
 // instruct the app to use the `bodyParser()` middleware for all routes
 app.use(bodyParser.urlencoded({
   extended: true
